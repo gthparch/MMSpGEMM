de85645d9e933c71df622c71e3fc5ac1835c4ae9
Eric Lorimer <eric.lorimer@gmail.com>
2023-08-25 23:16:56 -0400

    Bug fixes; still not correct number of outputs per block

--- gpu/load_balance_clean.cu
+++ gpu/load_balance_clean.cu
@@ -40,25 +40,33 @@ struct CustomLess
 };
 
 
+// XXX: TODO: Fix using 32-bit ints for pointers in kernel
+struct block_output_row
+{
+    uint64_t start;
+    uint32_t size;
+    float carry_out;
+    uint32_t key_in;
+    uint32_t key_out;
+};
+
 __global__ void cuda_load_block_coop(const int *AmRowPtrs, const int *AmColIdx, const float *AmCSRVals,
                                      const int *BmRowPtrs, const int *BmColIdx, const float *BmCSRVals,
                                      const int *lb_data, const int *lb_block_ptrs, const TSplit* lb_thread_splits, float *output,
-                                     int *out_keys, float *out_vals, int *atomic_p)
+                                     uint32_t *out_keys, float *out_vals, int *atomic_p, block_output_row *out_meta)
 {
     int block = blockIdx.x;
+//    int block = 0;
     int cur_block_ptr = lb_block_ptrs[block];
     int next_block_ptr = lb_block_ptrs[block+1];
     int end_row = lb_data[next_block_ptr];
     int start_row = lb_data[cur_block_ptr];
 
-    typedef cub::BlockRadixSort<int, NUM_THREADS, ITEMS_PER_THREAD, float, 4, true> BlockRadixSort;
-    typedef cub::BlockReduce<float, NUM_THREADS> BlockReduce;
+    typedef cub::BlockRadixSort<uint32_t, NUM_THREADS, ITEMS_PER_THREAD, float, 4, true> BlockRadixSort;
     __shared__ union {
         typename BlockRadixSort::TempStorage block_radix_storage;
-        typename BlockReduce::TempStorage block_reduce_storage;
-        float all_vals[BLOCK_SIZE];
     } smem;
-    int thread_keys[ITEMS_PER_THREAD];
+    uint32_t thread_keys[ITEMS_PER_THREAD];
     float thread_vals[ITEMS_PER_THREAD];
 
     TSplit split = lb_thread_splits[block * NUM_THREADS + threadIdx.x];
@@ -70,6 +78,7 @@ __global__ void cuda_load_block_coop(const int *AmRowPtrs, const int *AmColIdx, 
     if (split.a_row == end_row)
         seg_end = BmRowPtrs[brow] + lb_data[next_block_ptr + 2 + (split.bp - coeff_start)];
     float Acoeff = AmCSRVals[split.bp];
+//    printf("split.a_row = %d, split.bp = %d, split.b_col = %d, col_idx = %d, seg_end = %d\n", split.a_row, split.bp, split.b_col, BmColIdx[split.b_col], seg_end);
 
     for (int i = 0; i < ITEMS_PER_THREAD; i++)
     {
@@ -85,6 +94,10 @@ __global__ void cuda_load_block_coop(const int *AmRowPtrs, const int *AmColIdx, 
             brow = AmColIdx[split.bp] - 1;
             split.b_col = BmRowPtrs[brow];
             seg_end = BmRowPtrs[brow+1];
+            if (split.a_row == start_row) {
+                int coeff_start = AmRowPtrs[split.a_row];
+                split.b_col += lb_data[cur_block_ptr + 2 + (split.bp - coeff_start)];
+            }
             if (split.a_row == end_row) {
                 int coeff_start = AmRowPtrs[split.a_row];
                 seg_end = BmRowPtrs[brow] + lb_data[next_block_ptr + 2 + (split.bp - coeff_start)];
@@ -93,6 +106,7 @@ __global__ void cuda_load_block_coop(const int *AmRowPtrs, const int *AmColIdx, 
 
         thread_keys[i] = ((split.a_row - start_row) << 25) | BmColIdx[split.b_col];
         thread_vals[i] = BmCSRVals[split.b_col] * Acoeff;
+//        printf("tid = %d, thread_keys[%d] = %d, %d, %d, b_col = %d, vals = %f, %f, %f\n", threadIdx.x, i, split.a_row, BmColIdx[split.b_col], thread_keys[i], split.b_col, thread_vals[i], BmCSRVals[split.b_col], Acoeff);
         split.b_col++;
     }
 
@@ -104,7 +118,6 @@ __global__ void cuda_load_block_coop(const int *AmRowPtrs, const int *AmColIdx, 
 
 
 
-
 /************************************************
  * Reduction phase
  */
@@ -112,41 +125,40 @@ __global__ void cuda_load_block_coop(const int *AmRowPtrs, const int *AmColIdx, 
     int warp = threadIdx.x / 32;
     int lane = threadIdx.x & 31;
 
-    typedef cub::BlockLoad<int, NUM_THREADS, ITEMS_PER_THREAD> BlockLoadInt;
-    typedef cub::BlockLoad<float, NUM_THREADS, ITEMS_PER_THREAD> BlockLoadFloat;
-    typedef cub::BlockStore<int, NUM_THREADS, ITEMS_PER_THREAD> BlockStoreInt;
-    typedef cub::BlockStore<float, NUM_THREADS, ITEMS_PER_THREAD> BlockStoreFloat;
-
     // XXX: Hard-coded 4 warps
     // Could combine these in the union below ...?
 	__shared__ int delta_shared[4 + NUM_THREADS];	
     __shared__ float carry_out[NUM_THREADS];
-    __shared__ int block_keys[4 + 1];       // reuse delta_shared?
+    __shared__ uint32_t block_keys[4 + 1];       // reuse delta_shared?
+    block_keys[4] = 0xffffffff;
 
     if (lane == 0)
         block_keys[warp] = thread_keys[0];
 
     int p = 0;
-    float pcarry_out = 0;
+//    float pcarry_out = 0;
     for (int i=1; i < ITEMS_PER_THREAD; i++)
     {
         if (thread_keys[p] == thread_keys[i]) {
             thread_vals[p] += thread_vals[i];
-            pcarry_out = thread_vals[p];
+//            pcarry_out = thread_vals[p];
         }
         else {
             p++;
             thread_keys[p] = thread_keys[i];
             thread_vals[p] = thread_vals[i];
-            pcarry_out = 0;
+//            pcarry_out = 0;
         }
     }
-    carry_out[threadIdx.x] = pcarry_out;
+//    carry_out[threadIdx.x] = pcarry_out;
+    carry_out[threadIdx.x] = thread_vals[p];
 
-    int neighbor_key = __shfl_down_sync(FULL_MASK, thread_keys[0], 1);
+    __syncthreads();
+    uint32_t neighbor_key = __shfl_down_sync(FULL_MASK, thread_keys[0], 1);
     if (lane == 31) {
         neighbor_key = block_keys[warp+1];
     }
+//    __syncthreads();
     // what happens to the very last warp carry_out?
     bool rseg_end = (p > 0);
     if (thread_keys[ITEMS_PER_THREAD-1] != neighbor_key) {
@@ -154,6 +166,7 @@ __global__ void cuda_load_block_coop(const int *AmRowPtrs, const int *AmColIdx, 
         rseg_end = true;
         carry_out[threadIdx.x] = 0.0;
     }
+//    __syncthreads();
 
     uint warp_mask = FULL_MASK >> (31 - lane);
     uint cta_mask = 0x7fffffff >> (31 - lane);
@@ -201,16 +214,23 @@ __global__ void cuda_load_block_coop(const int *AmRowPtrs, const int *AmColIdx, 
 
     // TODO: coop write worth it?
     __shared__ int shared_p;
-    if (threadIdx.x == 0)
+    if (threadIdx.x == 0) {
         shared_p = atomicAdd(atomic_p, total_p);
+    }
     __syncthreads();
+
+    if (threadIdx.x == NUM_THREADS-1) {
+        out_meta[block].start = shared_p;
+        out_meta[block].size = total_p;
+        out_meta[block].carry_out = carry_out[threadIdx.x];
+        out_meta[block].key_out = thread_keys[ITEMS_PER_THREAD-1];
+    }
 
     start_p += shared_p;
     for (int i=0; i < p; i++) {
         out_keys[start_p + i] = thread_keys[i];
         out_vals[start_p + i] = thread_vals[i];
     }
-//    out_counts[threadIdx.x] = p;
 
 
 /*
@@ -288,8 +308,10 @@ int main(int argc, char **argv)
     mgpu::mem_t<TSplit> d_flat_tsplits = mgpu::to_mem(lb_flat_splits, context);
 
     std::vector<float> out_buffer;
-    std::vector<int> out_keys;
+    std::vector<uint32_t> out_keys;
     std::vector<float> out_vals;
+    std::vector<block_output_row> out_meta;
+    out_meta.resize(lb_block_ptrs.size());
     out_keys.resize(100000000);
     out_vals.resize(100000000);
     std::vector<int> atomic_p;
@@ -297,9 +319,10 @@ int main(int argc, char **argv)
 
     out_buffer.resize(lb_block_ptrs.size());
     mgpu::mem_t<float> d_output = mgpu::to_mem(out_buffer, context);
-    mgpu::mem_t<int> d_out_keys = mgpu::to_mem(out_keys, context);
+    mgpu::mem_t<uint32_t> d_out_keys = mgpu::to_mem(out_keys, context);
     mgpu::mem_t<float> d_out_vals = mgpu::to_mem(out_vals, context);
     mgpu::mem_t<int> d_atomic_p = mgpu::to_mem(atomic_p, context);
+    mgpu::mem_t<block_output_row> d_out_meta = mgpu::to_mem(out_meta, context);
 
     /*
     int numBlocks;
@@ -309,15 +332,51 @@ int main(int argc, char **argv)
 
     auto start = std::chrono::system_clock::now();
     cuda_load_block_coop<<<lb_block_ptrs.size()-1, NUM_THREADS>>>(dmA.raw.d_row_ptrs, dmA.raw.d_col_idx, dmA.raw.d_values,
+//    cuda_load_block_coop<<<1, NUM_THREADS>>>(dmA.raw.d_row_ptrs, dmA.raw.d_col_idx, dmA.raw.d_values,
                                              dmB.raw.d_row_ptrs, dmB.raw.d_col_idx, dmB.raw.d_values,
-                                             d_lb_data.data(), d_lb_block_ptrs.data(), d_flat_tsplits.data(), d_output.data(), d_out_keys.data(), d_out_vals.data(), d_atomic_p.data());
+                                             d_lb_data.data(), d_lb_block_ptrs.data(), d_flat_tsplits.data(), d_output.data(), d_out_keys.data(), d_out_vals.data(), d_atomic_p.data(),
+                                             d_out_meta.data());
     cudaDeviceSynchronize();
     auto finish = std::chrono::system_clock::now();
     double elapsed = std::chrono::duration<double, std::milli>(finish - start).count();
     std::cout << "elapsed: " << elapsed << std::endl;
 
+    /*
     std::vector<float> read_back = mgpu::from_mem(d_output);
     std::cout << "Result 0: " << read_back[0] << std::endl;
+    */
+
+
+    // print out the block output table
+    std::vector<block_output_row> block_table = mgpu::from_mem(d_out_meta);
+    out_keys = mgpu::from_mem(d_out_keys);
+    out_vals = mgpu::from_mem(d_out_vals);
+
+//    for (auto it = block_table.begin(); it != block_table.end(); ++it)
+    unsigned total = 0;
+    for (unsigned int i = 0; i < block_table.size(); i++)
+    {
+        std::cout << "Block " << i << ": " << block_table[i].start << "," << block_table[i].size << "," << block_table[i].carry_out << "," << block_table[i].key_in << "," << block_table[i].key_out << std::endl;
+        total += block_table[i].size;
+    }
+    std::cout << "total: " << total << std::endl;
+
+    // output block bp
+    int bp = 0;
+    for (int i = block_table[bp].start; i < block_table[bp].start + block_table[bp].size; i++)
+    {
+        int row = (out_keys[i] >> 25) + lb_data[lb_block_ptrs[bp]];
+        int col = out_keys[i] & ((1 << 25) - 1);
+        std::cout << "Block 0: " << (i - block_table[bp].start) << " : (" << row << ", " << col << "): " << out_vals[i] << std::endl;
+    }
+
+    /*
+    for (int i=0; i < block_table[0].size; i++)
+    {
+
+        std::cout << out_keys[i] << " = " << out_vals[i] << std::endl;
+    }
+    */
 
     return 0;
 }
